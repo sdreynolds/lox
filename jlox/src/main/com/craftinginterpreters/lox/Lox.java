493d6004e6744a5f3fbd6ddfd130375ec201cbb5
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-04-14 14:13:40 -0700

    Compiling Scanner implementation
    Summary:
    Up to chapter 4.5.2 where we start to add operators. This is a nice
    place to stop and pick up later

--- jlox/src/main/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/main/com/craftinginterpreters/lox/Lox.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -22,6 +24,10 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) {
+            System.exit(65);
+        }
     }
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
@@ -32,6 +38,7 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
     private static void run(String source) {
@@ -43,4 +50,16 @@ public class Lox {
             System.out.println(token);
         }
     }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
 }
