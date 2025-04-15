eb6a89470fe50a37f33b7811a3ba769f11537640
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-04-15 16:45:45 -0700

    First multicharacter tokens including strings and numbers
    This adds parsing for !=, //, and equality operators *and* it
    adds scanning for strings and numbers

--- jlox/src/main/com/craftinginterpreters/lox/Scanner.java
+++ jlox/src/main/com/craftinginterpreters/lox/Scanner.java
@@ -35,12 +35,115 @@ class Scanner {
         case '+': addToken(PLUS); break;
         case ';': addToken(SEMICOLON); break;
         case '*': addToken(STAR); break;
+        case '!':
+            addToken(match('=') ? BANG_EQUAL : BANG);
+            break ;
+        case '=':
+            addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            break;
+        case '<':
+            addToken(match('=') ? LESS_EQUAL : LESS);
+            break;
+        case '>':
+            addToken(match('=') ? GREATER_EQUAL : GREATER);
+            break;
+
+        case '/':
+            if (match('/')) {
+                // A comment! go to end of line
+                while (peek() != '\n' && !isAtEnd()) {
+                    advance();
+                }
+            } else {
+                addToken(SLASH);
+            }
+            break;
+
+        case ' ', '\r', '\t': break;
+        case '\n':
+            line++;
+            break;
+
+        case '"':
+            string();
+            break;
+
 
         default:
-            Lox.error(line, "Unexpected character.");
+            if (isDigit(c)) {
+                number();
+            } else {
+                Lox.error(line, "Unexpected character.");
+            }
             break;
         }
     }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+        }
+
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+        }
+
+        advance();
+
+        final String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) {
+            return '\0';
+        } else {
+            return source.charAt(current + 1);
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        } else if (source.charAt(current) != expected) {
+            return false;
+        } else {
+            current++;
+            return true;
+        }
+    }
+
     private char advance() {
         return source.charAt(current++);
     }
--- jlox/src/test/com/craftinginterpreters/lox/BasicTokensScanTest.java
+++ jlox/src/test/com/craftinginterpreters/lox/BasicTokensScanTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.Arguments;
 
 
@@ -16,35 +17,72 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class BasicTokensScanTest {
-    @DisplayName("A parameterized test with named argument sets")
-    @ParameterizedTest
-    @FieldSource("singleTokens")
-    public void validTokens(char character, TokenType expectedType) {
-        final Scanner scan = new Scanner(Character.toString(character));
-
-        assertIterableEquals(List.of(new Token(expectedType,
-                                               Character.toString(character), null, 1),
-                             new Token(TokenType.EOF, "", null, 1)),
-                             scan.scanTokens());
-    }
-
-    static List<Arguments> singleTokens = Arrays.asList(
-        arguments('(', TokenType.LEFT_PAREN),
-        arguments(')', TokenType.RIGHT_PAREN),
-        arguments('{', TokenType.LEFT_BRACE),
-        arguments('}', TokenType.RIGHT_BRACE),
-        arguments(',', TokenType.COMMA),
-        arguments('.', TokenType.DOT),
-        arguments('-', TokenType.MINUS),
-        arguments('+', TokenType.PLUS),
-        arguments(';', TokenType.SEMICOLON),
-        arguments('*', TokenType.STAR));
-
     @DisplayName("Invalid Token Test")
     @Test
     void invalidToken() {
         final Scanner scan = new Scanner("?");
         assertIterableEquals(List.of(new Token(TokenType.EOF, "", null, 1)),
+                             scan.scanTokens());
+    }
+
+    @DisplayName("Test all the single tokens from a single line")
+    @ParameterizedTest
+    @FieldSource("singleLineSingleTokens")
+    void singleLineTokens(final String line, final TokenType expectedType) {
+        final Scanner scan = new Scanner(line);
+        // We know this is fine for this case.
+        final int newLineCount = (int)line.chars().filter(ch -> ch == '\n').count();
+        assertIterableEquals(
+            List.of(new Token(expectedType, line.replaceAll("\\n", "").replace(" ", "").replace("\r", "").replace("\t", ""), null, 1),
+                    new Token(TokenType.EOF, "", null, newLineCount + 1)),
+            scan.scanTokens());
+    }
+
+    static List<Arguments> singleLineSingleTokens = Arrays.asList(
+        arguments("!=", TokenType.BANG_EQUAL),
+        arguments("!", TokenType.BANG),
+        arguments("=", TokenType.EQUAL),
+        arguments("==", TokenType.EQUAL_EQUAL),
+        arguments("<=", TokenType.LESS_EQUAL),
+        arguments("<", TokenType.LESS),
+        arguments(">", TokenType.GREATER),
+        arguments(">=", TokenType.GREATER_EQUAL),
+
+        arguments("(", TokenType.LEFT_PAREN),
+        arguments(")", TokenType.RIGHT_PAREN),
+        arguments("{", TokenType.LEFT_BRACE),
+        arguments("}", TokenType.RIGHT_BRACE),
+        arguments(",", TokenType.COMMA),
+        arguments(".", TokenType.DOT),
+        arguments("-", TokenType.MINUS),
+        arguments("+", TokenType.PLUS),
+        arguments(";", TokenType.SEMICOLON),
+        arguments("*", TokenType.STAR),
+        arguments("/", TokenType.SLASH),
+
+        arguments("     \t\r        (", TokenType.LEFT_PAREN)
+        );
+
+    @DisplayName("Test string literal parsing")
+    @Test
+    void stringLiteral() {
+        final Scanner scan = new Scanner("\"This is a test string\"");
+        assertIterableEquals(List.of(
+                                 new Token(TokenType.STRING, "\"This is a test string\"", "This is a test string" , 1),
+                                 new Token(TokenType.EOF, "", null, 1)),
+            scan.scanTokens());
+    }
+
+    @DisplayName("Tesging number literals")
+    @ParameterizedTest
+    @ValueSource(strings = {"12345", "12.34", "2.356", "1223500606.38"})
+    void numberLineTokens(final String line) {
+        final Scanner scan = new Scanner(line);
+        double expectedValue = Double.valueOf(line);
+
+        assertIterableEquals(List.of(
+                                 new Token(TokenType.NUMBER, line, expectedValue, 1),
+                                 new Token(TokenType.EOF, "", null, 1)),
                              scan.scanTokens());
     }
 }
