0f11d56273cea0e1e698cb61b8986b0fa2f65fd6
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-04-15 10:00:36 -0700

    Add test for basic scanning of single tokens

---
+++ jlox/src/test/com/craftinginterpreters/lox/BasicTokensScanTest.java
@@ -0,0 +1,42 @@
+package com.craftinginterpreters.lox;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.Arguments;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class BasicTokensScanTest {
+    @DisplayName("A parameterized test with named argument sets")
+    @ParameterizedTest
+    @FieldSource("singleTokens")
+    public void yep(char character, TokenType expectedType) {
+        final Scanner scan = new Scanner(Character.toString(character));
+
+        assertIterableEquals(List.of(new Token(expectedType,
+                                               Character.toString(character), null, 1),
+                             new Token(TokenType.EOF, "", null, 1)),
+                             scan.scanTokens());
+    }
+
+    static List<Arguments> singleTokens = Arrays.asList(
+        arguments('(', TokenType.LEFT_PAREN),
+        arguments(')', TokenType.RIGHT_PAREN),
+        arguments('{', TokenType.LEFT_BRACE),
+        arguments('}', TokenType.RIGHT_BRACE),
+        arguments(',', TokenType.COMMA),
+        arguments('.', TokenType.DOT),
+        arguments('-', TokenType.MINUS),
+        arguments('+', TokenType.PLUS),
+        arguments(';', TokenType.SEMICOLON),
+        arguments('*', TokenType.STAR));
+}
