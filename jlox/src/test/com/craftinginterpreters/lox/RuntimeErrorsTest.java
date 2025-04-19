ef12b02672b88aa41b03272c1232128e004c0194
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-04-19 11:33:43 -0700

    remove junit vintage engine

--- jlox/src/test/com/craftinginterpreters/lox/RuntimeErrorsTest.java
+++ jlox/src/test/com/craftinginterpreters/lox/RuntimeErrorsTest.java
@@ -3,10 +3,8 @@ package com.craftinginterpreters.lox;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.Assert.assertThat;
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrNormalized;
 
 class RuntimeErrorsTest {
@@ -18,7 +16,7 @@ class RuntimeErrorsTest {
             () -> new Interpreter().execute(
                 new Parser(new Scanner("2 * (3 / -\"muffin\");").scanTokens()).parse().get(0)),
             "Expected a Runtime Error on the unary negation");
-        assertThat(error.getMessage(), containsString("Operand must be a number."));
+        assertEquals(error.getMessage(), "Operand must be a number.");
     }
 
     @DisplayName("Multiply with String")
@@ -29,7 +27,7 @@ class RuntimeErrorsTest {
             () -> new Interpreter().execute(
                 new Parser(new Scanner("2 * \"is this a three and 0\";").scanTokens()).parse().get(0)),
             "Expected a Runtime Error when multiplying strings");
-        assertThat(error.getMessage(), containsString("Operands must be numbers."));
+        assertEquals(error.getMessage(), "Operands must be numbers.");
     }
 
     @DisplayName("Print variable before it is declared")
