861ec410c02fbbfdf1e7ccc6f831719ac3b939d8
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-06-14 09:24:43 -0700

    String parsing and error creation

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -87,6 +87,21 @@ static bool match(char expected) {
     return true;
 }
 
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') {
+            scanner.line++;
+        }
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    // The closing quote.
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
     skipWhitespace();
     scanner.start = scanner.current;
@@ -115,6 +130,8 @@ Token scanToken() {
         return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
     case '>':
         return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+
+    case '"': return string();
     }
 
     return errorToken("Unexpected character.");
--- clox/test/scanner/simple_tokens_test.cc
+++ clox/test/scanner/simple_tokens_test.cc
@@ -5,7 +5,7 @@ extern "C" {
 #include "clox/src/scanner.h"
 }
 
-TEST(SimpleTokens, SkipWhiteSpace) {
+TEST(SimpleToken, SkipWhiteSpace) {
     initScanner("        \n   \t    \t   \r\t\n");
     EXPECT_EQ(TOKEN_EOF, scanToken().type);
 }
@@ -27,4 +27,21 @@ TEST(SimpleToken, MatchLookAhead) {
 TEST(SimpleToken, NonMatch) {
     initScanner(">");
     EXPECT_EQ(TOKEN_GREATER, scanToken().type);
+}
+
+TEST(SimpleToken, String) {
+    const char* source = "\"Some String\"";
+    initScanner(source);
+    Token result = scanToken();
+    EXPECT_EQ(TOKEN_STRING, result.type);
+
+    // @TODO: add a assertation for the lexme.
+}
+
+TEST(SimpleToken, UnterminatedString) {
+    const char* source = "\"Some String";
+    initScanner(source);
+    Token result = scanToken();
+    EXPECT_EQ(TOKEN_ERROR, result.type);
+
 }
