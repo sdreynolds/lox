0b53fa2c0a78ecd72f2ecf7bf20a64cd6c4024fb
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-06-15 16:39:40 -0700

    Boolean handling for logic
    This adds NIL, TRUE, FALSE and the comparsion operators into the vm
    and the compiled byte code.

---
+++ clox/test/boolean/compare_test.cc
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "clox/src/vm.h"
+    #include "clox/src/common.h"
+}
+
+#undef DEBUG_TRACE_EXECUTION
+#undef DEBUG_PRINT_CODE
+
+TEST(Boolean, GreaterThan) {
+    testing::internal::CaptureStdout();
+    initVM();
+    interpret("(1 + 3) * 2 > -14");
+    std::string output = testing::internal::GetCapturedStdout();
+
+
+    // @TODO: would be nice if it didn't have all the debug info
+    EXPECT_EQ("== code ==\n0000    1 OP_CONSTANT      0 '1'\n0002    | OP_CONSTANT      1 '3'\n0004    | OP_ADD\n0005    | OP_CONSTANT      2 '2'\n0007    | OP_MULTIPLY\n0008    | OP_CONSTANT      3 '14'\n0010    | OP_NEGATE\n0011    | OP_GREATER\n0012    | OP_RETURN\n             \n0000    1 OP_CONSTANT      0 '1'\n             [ 1 ]\n0002    | OP_CONSTANT      1 '3'\n             [ 1 ][ 3 ]\n0004    | OP_ADD\n             [ 4 ]\n0005    | OP_CONSTANT      2 '2'\n             [ 4 ][ 2 ]\n0007    | OP_MULTIPLY\n             [ 8 ]\n0008    | OP_CONSTANT      3 '14'\n             [ 8 ][ 14 ]\n0010    | OP_NEGATE\n             [ 8 ][ -14 ]\n0011    | OP_GREATER\n             [ true ]\n0012    | OP_RETURN\ntrue\n", output);
+}
--- clox/test/calculator_test.cc
+++ clox/test/calculator_test.cc
@@ -10,17 +10,17 @@ TEST(CalculatorTest, AddAndDivideAndNegate) {
 
     Chunk chunk;
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 1.2);
+    int constant = addConstant(&chunk, NUMBER_VAL(1.2));
     writeChunk(&chunk, OP_CONSTANT, 789);
     writeChunk(&chunk, constant, 789);
 
-    constant = addConstant(&chunk, 3.4);
+    constant = addConstant(&chunk, NUMBER_VAL(3.4));
     writeChunk(&chunk, OP_CONSTANT, 789);
     writeChunk(&chunk, constant, 789);
 
     writeChunk(&chunk, OP_ADD, 789);
 
-    constant = addConstant(&chunk, 5.6);
+    constant = addConstant(&chunk, NUMBER_VAL(5.6));
     writeChunk(&chunk, OP_CONSTANT, 789);
     writeChunk(&chunk, constant, 789);
 
