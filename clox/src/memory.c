a05ab890e551c5bf9018cb5b4cfe29577533204f
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-07-26 11:15:18 -0700

    Fix Double Free Bug
    Missing break, had the function fall through and attempt to free
    again.

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -20,6 +20,7 @@ static void freeObject(Obj* object) {
         ObjFunction* function = (ObjFunction*) object;
         freeChunk(&function->chunk);
         FREE(ObjFunction, object);
+        break;
     }
     case OBJ_STRING: {
         ObjString* string = (ObjString*)object;
